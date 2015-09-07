93e4a80d6fbe49c5ae055c986e377b17fb4d4c0e
Jan Machacek <jan.machacek@gmail.com>
2015-09-07 21:58:40 +0100

    Updated Spark code to use the latest exercise model:

--- basic/src/main/scala/io/muvr/analytics/basic/SparkConfiguration.scala
+++ basic/src/main/scala/io/muvr/analytics/basic/SparkConfiguration.scala
@@ -8,7 +8,7 @@ object SparkConfiguration {
   lazy val sparkConf: SparkConf = {
     new SparkConf()
       .setAppName("Muvr Analytics")
-      .set("spark.cassandra.connection.host", "192.168.99.101")
+      .set("spark.cassandra.connection.host", Option(System.getProperty("CASSANDRA_JOURNAL_CPS")).getOrElse("192.168.99.103"))
       .set("spark.cassandra.query.retry.count", "0")
       .set("spark.cassandra.connection.timeout_ms", "1000")
       .set("spark.cassandra.connection.reconnection_delay_ms.max", "5000")
